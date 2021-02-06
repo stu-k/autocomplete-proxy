38010a7a768a00cc70c07ea24d9a0d77797de470
Stu Kilgore <stuart.kilgore@gmail.com>
2021-02-06 13:30:53 -0600

    Users controller, fetching users from api

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -13,10 +14,50 @@ func statusController(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "ok")
 }
 
+func usersController(w http.ResponseWriter, req *http.Request) {
+	queries, ok := req.URL.Query()["search"]
+	if !ok {
+		queries = []string{""}
+	}
+
+	r, err := http.NewRequest(http.MethodGet, "http://127.0.0.1:8080", nil)
+	if err != nil {
+		log.Printf("users controller: error creating request: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("searching for term %q", queries[0])
+
+	q := r.URL.Query()
+	q.Add("search", queries[0])
+	r.URL.RawQuery = q.Encode()
+
+	res, err := http.DefaultClient.Do(r)
+	if err != nil {
+		log.Printf("users controller: error making request: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("users controller: error reading request body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, string(body))
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/status", statusController)
+
+	router.HandleFunc("/users", usersController).
+		Methods(http.MethodGet)
 
 	log.Printf("Listening on port " + port)
 	log.Fatal(http.ListenAndServe(":" + port, router))
